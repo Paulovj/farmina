cb7a0f373318ff511d81ac0a757a55651dbb7784
Paulovj <paulovj1991@gmail.com>
2018-12-20 09:53:06 -0200

    correcao no calendario

--- src/assets/scripts/datepicker/index.js
+++ src/assets/scripts/datepicker/index.js
@@ -91,7 +91,6 @@ export default (function () {
                   // var atualDate  =  moment(atual).utc('America/Sao_Paulo').format("DD/MM/Y")
                 
                   if(arrayDias.indexOf(date.trim()) > -1) {
-                    // if(arrayDias.indexOf(date.trim()) > -1) {
                     // console.log(arrayData[arrayDias.indexOf(date)].split('-')[1]);
                     console.log(arrayData[arrayDias.indexOf(date)]);
                     return {
